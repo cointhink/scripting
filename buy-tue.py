ed058b7854579877e1b14152624828749b880091
Don Park <donp@donp.org>
2017-08-30 13:41:36 -0700

    tweak

--- buy-tue.py
+++ buy-tue.py
@@ -5,7 +5,7 @@ def init(cointhink):
     cointhink.trade()
 
 def eachDay(cointhink, date):
-    now = date.strftime("%Y-%m-%d %H:%M:%S"))
+    now = date.strftime("%Y-%m-%d %H:%M:%S")
     weekday = d.strftime("%a")
     if weekday == "Tue":
         cointhink.log("its Tuesday!")
