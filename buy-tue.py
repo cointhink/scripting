1216dee670958a93dfe161f73a4afcaa48ab6a5f
Don Park <donp@donp.org>
2017-08-30 13:25:18 -0700

    crazy about Tuesdays

--- buy-tue.py
+++ buy-tue.py
@@ -1,9 +1,13 @@
 # buy-weekly-tue
 
 def init(cointhink):
-    cointhink.log("script init")
+    cointhink.log("settings: "+str(cointhink.settings))
     cointhink.trade()
 
 def eachDay(cointhink, date):
-    cointhink.log("eachDay "+date.strftime("%Y-%m-%d %H:%M:%S"))
+    now = date.strftime("%Y-%m-%d %H:%M:%S"))
+    weekday = d.strftime("%a")
+    if weekday == "Tue":
+        cointhink.log("its Tuesday!")
+    cointhink.log("eachDay is "+weekday+". "+now)
     cointhink.trade()
