45dc2cabb6ff36e9bb62f1457d3d7c87964a538f
Don Park <donp@donp.org>
2017-08-31 10:26:49 -0700

    typo

--- buy-tue.py
+++ buy-tue.py
@@ -6,7 +6,7 @@ def init(cointhink):
 
 def eachDay(cointhink, date):
     now = date.strftime("%Y-%m-%d %H:%M:%S")
-    weekday = d.strftime("%a")
+    weekday = date.strftime("%a")
     if weekday == "Tue":
         cointhink.log("its Tuesday!")
     cointhink.log("eachDay is "+weekday+". "+now)
