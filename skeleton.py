ab7416e7564271e21c1ad049b2e8ebfe798d2055
Don Park <donp@donp.org>
2017-10-04 16:06:19 -0700

    blank script

---
+++ skeleton.py
@@ -0,0 +1,9 @@
+def init(cointhink):
+  cointhink.log('init(cointhink)')
+
+def market_prices(cointhink, prices):
+  cointhink.log('market_prices(cointhink, prices)')
+
+def each_day(cointhink, date):
+  cointhink.log('each_day(cointhink, date)')
+
