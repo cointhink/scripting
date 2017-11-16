b1f32675deef2bb9b070f57062a3451f5bf88ad5
Don Park <donp@donp.org>
2017-11-16 15:01:53 +0700

    the real signal-percentage

--- signal-percentage.py
+++ signal-percentage.py
@@ -1,30 +1,33 @@
-# signal-0.5%-5min
+# signal-percentage
 import datetime
 
 btc_last = False
+signal_percentage = 0
 
 def init(cointhink):
     cointhink.log("settings: "+str(cointhink.settings))
-    cointhink.notify("signal-0.5%-5min begin.")
+    cointhink.notify("signal-percentage begin.")
+    signal_percentage = float(cointhink.settings['percent_change'])
 
 def market_prices(cointhink, prices):
     global btc_last
+    global signal_percentage
     for price in prices.Prices:
         new_price = float(price.Amount)
         received_at = datetime.datetime.strptime(price.ReceivedAt, "%Y-%m-%dT%H:%M:%SZ")
-        if price.Market.lower() == cointhink.settings['Market']:
+        if price.Market.lower() == cointhink.settings['market']:
           if btc_last:
             price_delta = new_price - btc_last[0]
             chg_price_ratio = price_delta / new_price
             chg_time = received_at - btc_last[1]
-            log_msg = "{} price ${:.2f} changed ${:.2f} {:.2%} in {:.1f} mins".format(
-              cointhink.settings['Market'], new_price, price_delta, chg_price_ratio,
-              chg_time.seconds/60)
+            log_msg = "{} price ${:.2f} changed ${:.2f} {:.2%} in {}".format(
+              cointhink.settings['market'], new_price, price_delta, chg_price_ratio,
+              time_words(chg_time))
             cointhink.log(log_msg)
-            if abs(chg_price_ratio) > 0.005:
+            if abs(chg_price_ratio) > signal_percentage:
               cointhink.notify(log_msg)
           else:
-            log_msg = "{} first price ${}".format(cointhink.settings['Market'], new_price)
+            log_msg = "{} first price ${}".format(cointhink.settings['market'], new_price)
             cointhink.log(log_msg)
           btc_last = (new_price, received_at)
 
@@ -33,3 +36,11 @@ def each_day(cointhink, date):
     if btc_last:
       log_msg = log_msg + " btc_last price {} date {}".format(btc_last[0], btc_last[1])
     cointhink.log(log_msg)
+
+def time_words(time):
+    value = time.seconds
+    unit = "seconds"
+    if time.seconds > 60:
+      value = time.seconds / 60
+      unit = "minutes"
+    return "{:.1f} {}".format(value, unit)
